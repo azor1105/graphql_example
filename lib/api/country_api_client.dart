6377059c45939190a36b86ebcbb26b5a747c7acc
azor <azambekergashev1@gmail.com>
2022-10-18 04:06:36 +0500

    api client is completed

---
+++ lib/api/country_api_client.dart
@@ -0,0 +1,35 @@
+import 'package:graphql_example/api/models/models.dart' as models;
+import 'package:graphql_example/api/queries/queries.dart' as queries;
+import 'package:graphql/client.dart';
+
+class GetCountrysRequestFailure implements Exception {}
+
+class CountryApiClient {
+  const CountryApiClient({required GraphQLClient graphQLClient})
+      : _graphQLClient = graphQLClient;
+
+  factory CountryApiClient.create() {
+    final httpLink = HttpLink('https://countries.trevorblades.com');
+    final link = Link.from([httpLink]);
+    return CountryApiClient(
+      graphQLClient: GraphQLClient(cache: GraphQLCache(), link: link),
+    );
+  }
+
+  final GraphQLClient _graphQLClient;
+
+  Future<List<models.CountryCodeModel>> getJobs() async {
+    final result = await _graphQLClient.query(
+      QueryOptions(
+        document: gql(queries.getAllCountryInfoQuery),
+      ),
+    );
+    if (result.hasException) throw GetCountrysRequestFailure();
+    final data = result.data?['countries'] as List;
+    print(data);
+    return data
+        .map((dynamic country) =>
+            models.CountryCodeModel.fromJson(country as Map<String, dynamic>))
+        .toList();
+  }
+}
---
+++ lib/api/queries/queries.dart
@@ -0,0 +1,3 @@
+export 'get_all_countries.dart';
+export 'get_country_detail_info_by_code.dart';
+export 'get_min_country_info.dart';
